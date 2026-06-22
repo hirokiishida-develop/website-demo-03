cab8ab152c7b299ff50bcecfbd8d2a58aca24736
hiroki ishida <hiroki.ishida@acrospera.com>
2026-06-22 10:17:13 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { redirect } from 'next/navigation'
 
 export default function Page() {
-  redirect('/userselect');
+
 
   return (
-    <></>
+    <>
+      <h1>This is Home Page</h1>
+    </>
   );
 }

e7245fa17da52f195716dbb5832179aa8367758e
hiroki ishida <hiroki.ishida@acrospera.com>
2026-06-22 14:01:14 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
-import { redirect } from 'next/navigation'
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Page() {
-
-
+export default function HomePage(){
   return (
-    <>
-      <h1>This is Home Page</h1>
-    </>
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>Home Page (Test)</h1>
+      <p>この画面が表示されれば、ルーティングやフォルダ構成は正常です！</p>
+      <p>原因は MovieRow などのAPIフェッチ側のコードにあります。</p>
+    </div>
   );
 }
+// import { redirect } from 'next/navigation'
+
+// export default function Page() {
+
+
+//   return (
+//     <>
+//       <h1>This is Home Page</h1>
+//     </>
+//   );
+// }

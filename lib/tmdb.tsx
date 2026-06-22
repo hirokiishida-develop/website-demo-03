29f021a1969636543f98e710cf338ac72b8974ac
hiroki ishida <hiroki.ishida@acrospera.com>
2026-06-22 13:37:29 +0900

    text

--- lib/tmdb.tsx
+++ lib/tmdb.tsx
@@ -1,15 +1,18 @@
 export type MovieCategory = "now_playing" | "popular" | "top_rated" | "upcoming";
 
 export async function getMoviesByCategory(category: MovieCategory) {
-  const API_KEY = process.env.TMDB_API_KEY;
-  const BASE_URL = process.env.TMDB_BASE_URL;
+  // const API_KEY = process.env.TMDB_API_KEY;
+  // const BASE_URL = process.env.TMDB_BASE_URL;
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY || process.env.TMDB_API_KEY;
+  const baseUrl = process.env.TMDB_BASE_URL || 'https://api.themoviedb.org/3';
 
-  const res = await fetch(
-    `${BASE_URL}/movie/${category}?api_key=${API_KEY}&language=ja-JP&page=1`,
-    {
-      cache: "no-store",
-    }
-  );
+  // const res = await fetch(
+  //   `${BASE_URL}/movie/${category}?api_key=${API_KEY}&language=ja-JP&page=1`,
+  //   {
+  //     cache: "no-store",
+  //   }
+  // );
+  const res = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}`);
 
   if (!res.ok) {
     throw new Error(`Failed to fetch movies: ${category}`);
